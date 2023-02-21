3b30964712cf35068e1ff1e178cca85d7ff7da55
Henry <65220433+HenRMJ@users.noreply.github.com>
2023-02-20 18:35:26 -0800

    Lots of changes
    This is pulled from different people's githubs. I adjusted a few styling issues and added in Alt text but a large percentage of this work isn't mine

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
-// Expands the image when you click on it
-$(".asel-image-d").click(function() {
-  $(".asel-desktop-img").toggleClass("hides")
-})
+const fullOverlay = document.querySelector('.overlay-full-img');
+const projectImgs = document.querySelectorAll('.project-img');
+const imgContainers = document.querySelectorAll('.img-container');
+const highResImgs = document.querySelectorAll('.high-res-img');
 
-$(".asel-image-m").click(function() {
-  $(".asel-mobile-img").toggleClass("hides")
-})
+// display high resolution image
+imgContainers.forEach((container) => {
+  container.addEventListener('click', (e) => {
+    if (e.target.classList.contains('project-img')) {
+      // find the data-caption of the clicked element
+      const caption = e.target.dataset.caption;
+      //   remove hidden classes from overlay and high-res image
+      fullOverlay.classList.remove('hidden');
+      // find the element that has an id of data-caption's value that has been identified
+      document.querySelector(`#${caption}`).classList.remove('hidden');
+    }
+  });
+});
 
-$(".chess-image-d").click(function() {
-  $(".chess-desktop-img").toggleClass("hides")
-})
-
-$(".chess-image-m").click(function() {
-  $(".chess-mobile-img").toggleClass("hides")
-})
-
-$(".asel-desktop-img").click(function() {
-  $(".asel-desktop-img").toggleClass("hides")
-})
-
-$(".asel-mobile-img").click(function() {
-  $(".asel-mobile-img").toggleClass("hides")
-})
-
-$(".chess-desktop-img").click(function() {
-  $(".chess-desktop-img").toggleClass("hides")
-})
-
-$(".chess-mobile-img").click(function() {
-  $(".chess-mobile-img").toggleClass("hides")
-})
+// hide image:
+fullOverlay.addEventListener('click', (e) => {
+  highResImgs.forEach((img) => {
+    img.classList.add('hidden');
+  });
+  fullOverlay.classList.add('hidden');
+});
